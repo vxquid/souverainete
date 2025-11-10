12a12533edc59283cc9d1f284f17acbebe42e630
vxquid <pixel2376@gmail.com>
2025-11-10 05:18:40 +0500

    Fixed MusicDiscQuestItemStrategy.kt

--- src/main/kotlin/vx/ignis/gameplay/quest/pragma/strategy/MusicDiscQuestItemStrategy.kt
+++ src/main/kotlin/vx/ignis/gameplay/quest/pragma/strategy/MusicDiscQuestItemStrategy.kt
@@ -1,29 +1,24 @@
 package vx.ignis.gameplay.quest.pragma.strategy
 
+import org.bukkit.Material
 import org.bukkit.entity.LivingEntity
-import vx.ignis.Ignis.Companion.plugin
+import org.bukkit.inventory.ItemStack
 import vx.ignis.gameplay.dictionary.CustomItem
 import vx.ignis.gameplay.quest.pragma.QuestItemStrategy
 
+/**
+ * Simple quest for music disc search.
+ */
 class MusicDiscQuestItemStrategy : QuestItemStrategy() {
 
-    // Крайне простой квест. Квестовые предметы хранятся в пуле, который в будущем можно будет настроить.
     override fun get(questGiver: LivingEntity): CustomItem {
-        val fullKey = plugin.prompts.getStringList("item-gathering.allowed-types").random()
-        val parts = fullKey.split("~")
-        val key = parts[0]
-        val rangeStr = parts.getOrNull(1)
-        val quantity = if (rangeStr != null) {
-            val rangeParts = rangeStr.split("-")
-            val min = rangeParts[0].toInt()
-            val max = if (rangeParts.size > 1) rangeParts[1].toInt() else min
-            (min..max).random()
-        } else {
-            1
-        }
-        val item = dictionary.getItem(key)
-        item.item.amount = quantity
+        val disc = ItemStack(discs.random(), 1)
+        val item = CustomItem(disc.type.toString(), 3000, disc)
         return item
+    }
+
+    companion object {
+        val discs = Material.entries.filter { material: Material -> material.isRecord }
     }
 
 }
